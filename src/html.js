5bd78f2af3de9b1e296a29818c082e8692f9393b
nacknime-official <lenovo.maks@gmail.com>
2020-05-23 13:00:22 +0300

    add frealancehunt meta tag

--- src/html.js
+++ src/html.js
@@ -16,6 +16,7 @@ export default function HTML({
   return (
     <html {...htmlAttributes}>
       <head>
+        <meta name="freelancehunt" content="1e1abf9d60645d7" />
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
